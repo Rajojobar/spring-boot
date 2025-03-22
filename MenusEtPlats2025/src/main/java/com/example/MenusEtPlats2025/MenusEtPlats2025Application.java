5b628cbc35d620890bb191d17a7a3511ada25cb5
Raphaël BAR <raphaelbar003e@gmail.com>
2025-03-22 15:54:25 +0100

    Premier commit lancement du projet et paramétrage de l'accès BDD dans src/main/resources/application.properties

---
+++ MenusEtPlats2025/src/main/java/com/example/MenusEtPlats2025/MenusEtPlats2025Application.java
@@ -0,0 +1,13 @@
+package com.example.MenusEtPlats2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenusEtPlats2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenusEtPlats2025Application.class, args);
+	}
+
+}
---
+++ MenusEtPlats2025/src/test/java/com/example/MenusEtPlats2025/MenusEtPlats2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MenusEtPlats2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenusEtPlats2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
